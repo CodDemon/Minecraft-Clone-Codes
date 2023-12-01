56396eb2026ee3a163fc37946272475dbcfaccd8
CodDemon <151961439+CodDemon@users.noreply.github.com>
2023-12-01 23:07:19 +0400

    Add files via upload

---
+++ Minecraft.py
@@ -0,0 +1,61 @@
+from ursina import*
+from ursina.prefabs.first_person_controller import*
+FirstPersonController
+
+chosenBlock = 1
+
+def update():
+    global chosenBlock
+
+    if held_keys['1']:
+        chosenBlock = 1
+        print("Beyaz kup secildi")
+    if held_keys['2']:
+        chosenBlock = 2
+        print("cimen secildi")
+    if held_keys['3']:
+        chosenBlock = 3
+        print("tas secildi")
+
+class sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'sphere',
+            texture = 'sky_default',
+            double_sided = True,
+            scale = 150,
+        )
+
+class Block(Button):
+    def __init__(self, position=(0,0,0), texture = 'grass'):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            texture = texture,
+            color = color.white,
+            origin_y = .5,
+            highlight_color = color.lime,
+        )
+
+    def input(self,key):
+        if self.hovered:
+            if key == 'right mouse down':
+                if chosenBlock == 1:
+                    block = Block(position = self.position + mouse.normal,texture='white_cube')
+                if chosenBlock == 2:
+                    block = Block(position = self.position + mouse.normal, texture = 'grass')
+            if key == 'left mouse down':
+                destroy(self)
+
+app = Ursina()
+
+player = FirstPersonController()
+sky = Sky()
+
+for x in range(25):
+    for y in range(25):
+        block = Block(position = (x,0,y))
+
+app.run()
